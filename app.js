bdd3ebc157125b7511f15d9395a0451784e1350c
akshay_2304 <akshay.bhargava@jellyfishtechnologies.com>
2021-03-15 22:31:48 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express       = require('express')
+const bodyParser    = require('body-parser')
+const mongoose      = require('mongoose')
+const PORT          = process.env.PORT || 5000
+const cors          = require('cors')
+
+//Initialize the environment variable
+require('dotenv').config()
+
+// Initialize App
+const app = express()
+
+// Application Configuration
+app.use(bodyParser.urlencoded({ extended : true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+    }).then(res=>{ app.listen(PORT, ()=>{ console.log(`app listening at port number ${PORT}`)  })})
+      .catch(err=>{  console.log(err) })
